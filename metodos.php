db0de164431185a6e3ec4c2117ddf9216b0b4b9d
Kosmar-H <106706956+Kosmar-H@users.noreply.github.com>
2022-08-31 17:43:19 -0500

    Update metodos.php

--- metodos.php
+++ metodos.php
@@ -1,57 +1,56 @@
-
-    <?php  
+<?php 
+    function creacion_archivo_cv($ruta,$arreglo){
+        $encapsulador='"'; 
+        $delimitador=',';
+        $file_handle = fopen($ruta, 'w');
+        foreach ($arreglo as $linea) {
+            fputcsv($file_handle, $linea, $delimitador, $encapsulador);
+        }
+        rewind($file_handle);
+        fclose($file_handle); 
+        ?>
+        <a href="./index.php" title="Ir la página anterior">Volver</a>
+        <?php
+    }
+    function Estado_alumno($fp_alumnos,$datos_distribucion){
+        while ($datos_alumnos = fgetcsv($fp_alumnos, 0, ",")){//obtenemos datos del segundo csv por filas
+            if ($datos_distribucion[0]==$datos_alumnos[1]){ // comparamos los codigos del archivo1 y los del archivo2
+                return true;// si se repite estadoalumno se vuelve true
+                break;
+            }
+        }
+        return false;
+    } 
     class Metodos
     {
         public function no_tutorados($fp_distribucion,$archivo_copiado_alumnos)
         {
                     $indice=0;
                     $ruta ="no_matriculados.csv"; //ruta
-                    $encapsulador='"'; 
-                    $delimitador=',';
                     while ($datos_distribucion = fgetcsv($fp_distribucion, 0, ",")){ //obtenemos datos del primer csv por filas
-                        $EstadoAlumno=(boolean)false; // variable booleana igual a "false"
                         $fp_alumnos = fopen($archivo_copiado_alumnos, "r");// abrir el archivo2
-                        while ($datos_alumnos = fgetcsv($fp_alumnos, 0, ",")){//obtenemos datos del segundo csv por filas
-                            if ($datos_distribucion[0]==$datos_alumnos[1]){ // comparamos los codigos del archivo1 y los del archivo2
-                                $EstadoAlumno=(boolean)true;// si se repite estadoalumno se vuelve true
-                                break;
-                            }
-                        }
+                        $EstadoAlumno = Estado_alumno($fp_alumnos,$datos_distribucion);
                         if ($EstadoAlumno==(boolean)false && (integer)$datos_distribucion[0]){//si estado no cambia a false y no son alumnos nuevos y la primera columna es entera
                                 $arreglo[$indice] = array($datos_distribucion[0],$datos_distribucion[1],"No matriculado");
-                                $indice=$indice+1;
+                                $indice++;
                         }
                         fclose($fp_alumnos);// cerramos el archivo2
                         
                     }
                     //CREACION DE ARCHIVO CSV
-                    $file_handle = fopen($ruta, 'a');
-                    foreach ($arreglo as $linea) {
-                        fputcsv($file_handle, $linea, $delimitador, $encapsulador);
-                    }
-                    rewind($file_handle);
-                    fclose($file_handle); 
-                    ?>
-                    <a href="./index.php" title="Ir la página anterior">Volver</a>
-            <?php
+                    creacion_archivo_cv($ruta,$arreglo);
         }
         public function distribucion_balanceada($fp_distribucion,$archivo_copiado_alumnos,$archivo_copiado_docentes)
         {   
                 $indice=0;
                 $ruta ="distribucion_balanceada.csv"; //ruta
-                $encapsulador='"'; 
-                $delimitador=',';
+                
             
                     while ($datos_distribucion = fgetcsv($fp_distribucion, 0, ",")){ //obtenemos datos del primer csv por filas
                         $EstadoAlumno=(boolean)false; // variable booleana igual a "false"
                         $fp_alumnos = fopen($archivo_copiado_alumnos, "r");// abrir el archivo2
                         $fp_docentes = fopen($archivo_copiado_docentes, "r");// abrir el archivo3
-                        while ($datos_alumnos = fgetcsv($fp_alumnos, 0, ",")){//obtenemos datos del segundo csv por filas
-                            if ($datos_distribucion[0]==$datos_alumnos[1]){ // comparamos los codigos del archivo1 y los del archivo2
-                                $EstadoAlumno=(boolean)true;// si se repite estadoalumno se vuelve true
-                                break;
-                            }
-                        }
+                        $EstadoAlumno = Estado_alumno($fp_alumnos,$datos_distribucion);
                         for ($i=0;$i<36;$i = $i + 1) {
                             if ($datos_distribucion[0]==("Docente #".$i)){
                                 $arreglo[$indice] = array($datos_distribucion[0],$datos_distribucion[1],"TUTOR");
@@ -63,22 +62,14 @@
                             }
                         }
                         if ($EstadoAlumno==(boolean)true && (integer)$datos_distribucion[0]){//si estado no cambia a false y no son alumnos nuevos y la primera columna es entera
-                            $arreglo[$indice] = array($datos_distribucion[0],$datos_alumnos[2],"Tutorados");
+                            $arreglo[$indice] = array($datos_distribucion[0],$datos_distribucion[1],"Tutorados");
                             $indice=$indice+1;
                         }
                         fclose($fp_alumnos);// cerramos el archivo2
                         fclose($fp_docentes);// cerramos el archivo3
                     }   
                     //CREACION DE ARCHIVO CSV
-                    $file_handle = fopen($ruta, 'a');
-                    foreach ($arreglo as $linea) {
-                        fputcsv($file_handle, $linea, $delimitador, $encapsulador);
-                    }
-                    rewind($file_handle);
-                    fclose($file_handle);  
-                    ?>
-                    <a href="./index.php" title="Ir la página anterior">Volver</a>
-            <?php      
+                    creacion_archivo_cv($ruta,$arreglo);   
         }
     }
     ?>
